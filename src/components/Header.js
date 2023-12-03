ba1ab5c7a6ee3cb3469586e3a8d58971f62ce2e9
Ismael-Raveloson <120553853+Ismael-Raveloson@users.noreply.github.com>
2023-12-03 13:30:49 +0300

    link

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,9 @@ function Tableau() {
       <div className ="header">
       <span className ="logo">StatFoot</span>
         <div className ="link">
-            <p><Link to="./">Général</Link></p>
-            <p><Link to="./attaque">Attaque</Link></p>
-            <p><Link to="./defense">Défense</Link></p>
+            <p><Link to="/">Général</Link></p>
+            <p><Link to="/attaque">Attaque</Link></p>
+            <p><Link to="/defense">Défense</Link></p>
         </div>
       </div>
     );
