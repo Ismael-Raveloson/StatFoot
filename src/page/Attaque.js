87589aed9632fcd6ad5cf679d907e71f3b093aa3
Ismael-Raveloson <120553853+Ismael-Raveloson@users.noreply.github.com>
2023-12-03 18:51:28 +0300

    correction deployment

--- src/page/Attaque.js
+++ src/page/Attaque.js
@@ -6,12 +6,7 @@ import {useEffect,useState } from "react";
 
 function Attaque() {
     const attaque = ["Equipe","Ligue","Tir/Match","Tir Cadre / Match","Dribble / Match","Faute Subie / Match","Note"];
-    const attaqueStat = [
-      {id:1,equipe:"Wolves",ligue:"Premier League", tirMatch:"11.00" , tirCadreMatch:"3.50", dribbleMatch:"17.20", fauteSubieMatch: "10.70",note:"6.60"},
-      {id:2,equipe:"Bayern Munich",ligue:"Bundesliga", tirMatch:"19.95" , tirCadreMatch:"8.30", dribbleMatch:"15.65", fauteSubieMatch: "9.30",note:"7.16"},
-      {id:3,equipe:"Nice",ligue:"Ligue 1", tirMatch:"12.90" , tirCadreMatch:"3.10", dribbleMatch:"14.10", fauteSubieMatch: "11.30",note:"6.78"}
-    ];
-
+    
     //fonction maka anle data dia omena url
     function getDataFromUrl(url) {
       return new Promise((resolve, reject) => {
--- src/page/Defense.js
+++ src/page/Defense.js
@@ -4,11 +4,6 @@ import {useEffect,useState } from "react";
 
 function Defense() {
   const defense = ["Equipe","Ligue","Tir / Match","Tacle / Match","Interception / Match","Faute / Match","Hors Jeu / Match","Note"];
-  const defenseStat = [
-    {id:1,equipe:"Mallorca",ligue:"Laliga", tirMatch:"11.00", tacleMatch:"22.40", interceptionMatch:"9.00", fauteMatch:"14.00", horsJeuxMatch:"5.60",note:"6.59"},
-    {id:2,equipe:"Brest",ligue:"Ligue 1", tirMatch:"13.10", tacleMatch:"22.10", interceptionMatch:"10.00", fauteMatch:"12.30", horsJeuxMatch:"1.40",note:"6.61"},
-    {id:3,equipe:"Valencia",ligue:"Laliga", tirMatch:"12.10", tacleMatch:"22.10", interceptionMatch:"9.00", fauteMatch:"13.10", horsJeuxMatch:"0.70",note:"6.50"}
-  ];
 
   function getDataFromUrl(url) {
     return new Promise((resolve, reject) => {
